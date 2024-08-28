8f6dd81817b837c0a420cfc3f0ffbe146acaf97f
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-08-28 18:55:36 +0200

    Add eval and update class name ordering

--- src/intrab/experiments.py
+++ src/intrab/experiments.py
@@ -60,13 +60,15 @@ def run_experiments(
     targets: dict = {k.replace("/", "_"): v for k, v in label_dict.items() if k != "background"}
 
     [
-        Path(results_dir / p.name / target).mkdir(exist_ok=True, parents=True)
+        Path(results_dir / p.name / (f"{target_label:03d}" + "__" + target)).mkdir(exist_ok=True, parents=True)
         for p in prompters
-        for target in targets.keys()
+        for target, target_label in targets.items()
     ]
 
     [
-        Path(results_dir / "binarised_gts" / (target + "__" + f"{target_label}")).mkdir(exist_ok=True, parents=True)
+        Path(results_dir / "binarised_gts" / (f"{target_label:03d}" + "__" + target)).mkdir(
+            exist_ok=True, parents=True
+        )
         for target, target_label in targets.items()
     ]
 
@@ -74,13 +76,13 @@ def run_experiments(
 
     # Loop through all image and label pairs
     target_names: set[str] = set()
-    for img_path, gt_path in tqdm(imgs_gts, desc="looping through files\n"):
+    for img_path, gt_path in tqdm(imgs_gts, desc="looping through files\n", leave=True):
         base_name = os.path.basename(gt_path)
         multi_class_gt = inferer.get_transformed_groundtruth(gt_path)
 
         # Loop through each organ label except the background
-        for target, target_label in tqdm(targets.items(), desc="looping through organs\n"):
-            target_name: str = target + "__" + f"{target_label}"
+        for target, target_label in tqdm(targets.items(), desc="Predicting targets...\n", leave=False):
+            target_name: str = f"{target_label:03d}" + "__" + target
             target_names.add(target_name)
             binary_gt = np.where(multi_class_gt == target_label, 1, 0)
             # Save the binarised ground truth next to the predictions for easy access -- Needed for evaluation
@@ -117,9 +119,11 @@ def run_experiments(
     for target_name in target_names:
         for prompter in prompters:
             if prompter.is_static:
-                semantic_evaluation(
-                    semantic_pd_path=results_dir / "binarised_gts" / target_name,
-                    semantic_gt_path=results_dir / prompter.name / target_name,
-                    output_path=results_dir / prompter.name,
-                    classes_of_interest=(1,),
-                )
+                with logger.catch(level="WARNING"):
+                    semantic_evaluation(
+                        semantic_pd_path=results_dir / "binarised_gts" / target_name,
+                        semantic_gt_path=results_dir / prompter.name / target_name,
+                        output_path=results_dir / prompter.name,
+                        classes_of_interest=(1,),
+                        output_name=target_name
+                    )
