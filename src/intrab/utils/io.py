e5c4505dd958b326053a1a5734cdb36e169e80dc
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-09-02 18:54:32 +0200

    Just formatting changes

--- src/intrab/utils/io.py
+++ src/intrab/utils/io.py
@@ -11,11 +11,13 @@ import nibabel as nib
 import numpy as np
 
 
-
 def get_matching_datasets(dataset_path: Path, dataset_id: int) -> list[Path]:
     """Finds all matching datasets by its id and returns a list of paths to that directory."""
-    matched_datasets = [dataset_path / d for d in os.listdir(dataset_path) if d.startswith(f"Dataset{dataset_id:03d}")]
+    matched_datasets = [
+        dataset_path / d for d in os.listdir(dataset_path) if d.startswith(f"Dataset{dataset_id:03d}")
+    ]
     return matched_datasets
+
 
 def get_dataset_path_by_id(dataset_id: int) -> Path:
     """Finds the dataset path by its id."""
@@ -23,16 +25,19 @@ def get_dataset_path_by_id(dataset_id: int) -> Path:
     found_matching_datasets = get_matching_datasets(dataset_path, dataset_id)[0]
     return found_matching_datasets
 
+
 def verify_dataset_exists(dataset_id: int) -> None:
     """
     Verify that the dataset exists.
     """
     dataset_path = get_dataset_path()
     found_matching_datasets = get_matching_datasets(dataset_path, dataset_id)
-    assert len(found_matching_datasets) > 0, f"No datasets found in {dataset_path}."
+    assert len(found_matching_datasets) > 0, f"No datasets found in '{dataset_path}'."
     assert len(found_matching_datasets) == 1, f"Multiple datasets found in {dataset_path}."
     assert found_matching_datasets[0].is_dir(), f"Dataset {dataset_id} is not a directory."
-    assert (found_matching_datasets[0] / "dataset.json").exists(), f"Dataset {dataset_id} does not have a dataset.json file."
+    assert (
+        found_matching_datasets[0] / "dataset.json"
+    ).exists(), f"Dataset {dataset_id} does not have a dataset.json file."
     return
 
 
@@ -44,7 +49,10 @@ def sanity_check_dataset_config(dataset_conf: dict) -> None:
     assert len(dataset_conf) > 0, "No datasets provided in the config file. Please provide at least one dataset."
     for dataset in dataset_conf:
         assert "identifier" in dataset.keys(), f"Dataset identifier is missing: {dataset}."
-        assert dataset["type"] in ["organ", "lesion"], f"Dataset {dataset['identifier']} type has to be either organ dataset or lesion dataset."
+        assert dataset["type"] in [
+            "organ",
+            "lesion",
+        ], f"Dataset {dataset['identifier']} type has to be either organ dataset or lesion dataset."
         verify_dataset_exists(dataset["identifier"])
     return
 
@@ -56,8 +64,11 @@ def sanity_check_model_config(models_conf) -> None:
     """
     assert len(models_conf) > 0, "No models provided in the config file. Please provide at least one model."
     for model in models_conf:
-        assert model in get_args(model_registry), f"Model {model} is not supported. Choose from {get_args(model_registry)}."
+        assert model in get_args(
+            model_registry
+        ), f"Model {model} is not supported. Choose from {get_args(model_registry)}."
     return
+
 
 def sanity_check_prompting(prompt_config: dict) -> None:
     """
@@ -68,8 +79,10 @@ def sanity_check_prompting(prompt_config: dict) -> None:
     assert len(prompt_config["prompt_styles"]) > 0, "No prompt styles provided."
     if prompt_config["type"] == "static":
         for p in prompt_config["prompt_styles"]:
-            assert p in get_args(static_prompt_styles), f"Prompt style {p} is not supported. Choose from {get_args(static_prompt_styles)}." 
-    
+            assert p in get_args(
+                static_prompt_styles
+            ), f"Prompt style {p} is not supported. Choose from {get_args(static_prompt_styles)}."
+
 
 def sanity_check_config(config: dict) -> None:
     """
@@ -79,7 +92,6 @@ def sanity_check_config(config: dict) -> None:
     sanity_check_dataset_config(config["datasets"])
     sanity_check_model_config(config["models"])
     sanity_check_prompting(config["prompting"])
-    
 
 
 def read_yaml_config(config_path: str) -> dict:
@@ -126,6 +138,7 @@ def verify_results_dir_exist(targets, results_dir, prompters: list[Prompter]):
         for target, target_label in targets.items()
     ]
 
+
 def get_labels_from_dataset_json(dataset_dir: Path) -> dict[str:int]:
     """
     Reads the dataset_json and returns a label dict of {class_name: class_id}
@@ -137,6 +150,7 @@ def get_labels_from_dataset_json(dataset_dir: Path) -> dict[str:int]:
         dataset_info = json.load(f)
     label_dict = dataset_info["labels"]
     return label_dict
+
 
 def binarize_gt(gt_path: Path, label_of_interest: int):
     """
