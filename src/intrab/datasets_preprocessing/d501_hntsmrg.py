20982d305e58ebe128f0620fb08ccd42d05646a8
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-09-11 19:13:07 +0200

    Add preprocessing for HNTSMRG  dataset

--- src/intrab/datasets_preprocessing/d501_hntsmrg.py
+++ src/intrab/datasets_preprocessing/d501_hntsmrg.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 import shutil
 from toinstance import InstanceNrrd
+import glob
 
 from tqdm import tqdm
 
@@ -12,10 +13,63 @@ from intrab.datasets_preprocessing.utils import (
     download_from_zenodo,
 )
 from intrab.utils.io import get_dataset_path_by_id
+from intrab.utils.paths import get_dataset_path
+import SimpleITK as sitk
+from tempfile import TemporaryDirectory
 
 
-def dataset_download():
-    zenodo_id = "11199559"
+def preprocess(raw_dataset_path: Path):
+
+    # This dataset contains two semantic:
+    # - Primary Tumor (Class 1)
+    # - Metastatic Lymph Nodes (Class 2)
+    # We will create instances of the first semantic class through connected components.
+    #   Adding the class of metastatic lymph nodes can be done later if necessary.
+
+    # IMPORTANT: In the dataset description they mention at MOST one primary tumor per patient. (Class 1)
+    #   This means no Connected Component analysis has to be done for Class 1
+    # ! IF THIS GETS EXTENDED TO CLASS 2 A CC NEEDS TO BE ADDED !
+
+    # Did some mess up with the folder structure, but whatever.
+    data_path = raw_dataset_path / "HNTSMRG24_train" / "HNTSMRG24_train"
+    target_path = get_dataset_path() / "Dataset501_hntsmrg_pre_primarytumor"
+    (target_path / "imagesTr").mkdir(parents=True, exist_ok=True)
+    (target_path / "labelsTr").mkdir(parents=True, exist_ok=True)
+
+    img_paths = list(data_path.rglob("preRT/*preRT_T2.nii.gz"))
+    lbl_paths = list(data_path.rglob("preRT/*preRT_mask.nii.gz"))
+
+    for img in tqdm(img_paths, desc="Copying images"):
+        target_img_name = target_path / "imagesTr" / (Path(img).name.replace(".nii.gz", "_0000.nii.gz"))
+        if not target_img_name.exists():
+            shutil.copy(img, target_img_name)
+
+    for lbl in tqdm(lbl_paths, desc="Copying labels"):
+        target_lbl_name = target_path / "labelsTr" / (Path(lbl).name)
+        if not target_lbl_name.exists():
+            with TemporaryDirectory() as tempdir:
+                img = sitk.ReadImage(lbl)
+                img_array = sitk.GetArrayFromImage(img)
+                # Remove the second label
+                img_array[img_array == 2] = 0
+                new_img = sitk.GetImageFromArray(img_array)
+                new_img.CopyInformation(img)
+                sitk.WriteImage(new_img, target_lbl_name)
+
+    # ------------------------------- Dataset Json ------------------------------- #
+    with open(get_dataset_path() / "Dataset501_hntsmrg_pre_primarytumor" / "dataset.json", "w") as f:
+        json.dump(
+            {
+                "channel_names": {"0": "CT"},
+                "labels": {"background": 0, "Primary Tumor": 1},
+                "numTraining": 150,
+                "file_ending": ".nii.gz",
+                "name": "HNTS-MRG 24 Challenge - Primary Tumor instances",
+                "reference": "https://hntsmrg24.grand-challenge.org/dataset/",
+                "release": "https://zenodo.org/records/11199559",
+            },
+            f,
+        )
 
 
 def main():
