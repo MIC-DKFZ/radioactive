4fd90549524a3536aa02b98dd10ccacb1dc05329
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-09-16 17:32:51 +0200

    Add static positive click prompt thingies

--- src/intrab/prompts/prompter.py
+++ src/intrab/prompts/prompter.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from functools import partial
 from pathlib import Path
 from typing import Literal
@@ -25,7 +25,11 @@ from intrab.prompts.prompt_utils import (
 )
 
 from intrab.utils.result_data import PromptResult
-from intrab.utils.transforms import canonical_to_orig_sparse_coords, orig_to_SAR_dense, transform_prompt_to_model_coords
+from intrab.utils.transforms import (
+    canonical_to_orig_sparse_coords,
+    orig_to_SAR_dense,
+    transform_prompt_to_model_coords,
+)
 
 
 # ToDo: Save the Prompt before feeding into the model.
@@ -104,7 +108,9 @@ class Prompter:
         prompt: PromptStep = self.get_prompt()
         pred: nib.Nifti1Image
         logits: np.ndarray
-        pred, logits, _ = self.inferer.predict(prompt, promptstep_in_model_coord_system=self.promptstep_in_model_coord_system)
+        pred, logits, _ = self.inferer.predict(
+            prompt, promptstep_in_model_coord_system=self.promptstep_in_model_coord_system
+        )
         perf = self.get_performance(pred.get_fdata())
 
         return PromptResult(predicted_image=pred, logits=logits, prompt_step=prompt, perf=perf, n_step=0, dof=0)
@@ -114,14 +120,18 @@ class Prompter:
         pass
 
     def transform_prompt_to_original_coords(self, prompt_orig: PromptStep) -> PromptStep:
-        sparse_transform = partial(canonical_to_orig_sparse_coords, orig_affine = self.orig_affine, orig_shape = self.orig_shape)
+        sparse_transform = partial(
+            canonical_to_orig_sparse_coords, orig_affine=self.orig_affine, orig_shape=self.orig_shape
+        )
         return transform_prompt_to_model_coords(prompt_orig, sparse_transform)
 
 
-class NPointsPer2DSlicePrompter(Prompter):
-    def __init__(self, inferer: Inferer, seed: int = 11111, n_points_per_slice: int = 5):
+class NFGPointsPer2DSlicePrompter(Prompter, ABC):
+    n_point_per_slice: int
+
+    def __init__(self, inferer: Inferer, seed: int = 11111):
         super().__init__(inferer, seed)
-        self.n_points_per_slice = n_points_per_slice
+        self.n_points_per_slice = self.n_point_per_slice
 
     def get_prompt(self) -> PromptStep:
         """
@@ -131,7 +141,28 @@ class NPointsPer2DSlicePrompter(Prompter):
         prompt_RAS = get_pos_clicks2D_row_major(self.groundtruth_SAR, self.n_points_per_slice, self.seed)
         prompt_orig = self.transform_prompt_to_original_coords(prompt_RAS)
         return prompt_orig
-    
+
+
+class OneFGPointsPer2DSlicePrompter(NFGPointsPer2DSlicePrompter):
+    n_point_per_slice = 1
+
+
+class TwoFGPointsPer2DSlicePrompter(NFGPointsPer2DSlicePrompter):
+    n_point_per_slice = 2
+
+
+class ThreeFGPointsPer2DSlicePrompter(NFGPointsPer2DSlicePrompter):
+    n_point_per_slice = 3
+
+
+class FiveFGPointsPer2DSlicePrompter(NFGPointsPer2DSlicePrompter):
+    n_point_per_slice = 5
+
+
+class TenFGPointsPer2DSlicePrompter(NFGPointsPer2DSlicePrompter):
+    n_point_per_slice = 10
+
+
 class CenterPointPrompter(Prompter):
     def get_prompt(self) -> PromptStep:
         """
@@ -141,18 +172,18 @@ class CenterPointPrompter(Prompter):
         volume_fg = np.where(self.groundtruth_SAR == 1)  # Get foreground indices (formatted as triple of arrays)
         volume_fg = np.array(volume_fg).T  # Reformat to numpy array of shape n_fg_voxels x 3
 
-        fg_slices = np.unique(
-            volume_fg[:, 0]
-        )
+        fg_slices = np.unique(volume_fg[:, 0])
 
         slice_prompts = []
         for slice_idx in fg_slices:
             gt_slice = self.groundtruth_SAR[slice_idx]
             slice_prompt = get_fg_point_from_cc_center(gt_slice)
-            slice_prompts.append(np.array([slice_prompt[1], slice_prompt[0], slice_idx])) # reverse order of slice_prompt since it's an axial slice from SAR, and add 3d context to put prompt into xyz
+            slice_prompts.append(
+                np.array([slice_prompt[1], slice_prompt[0], slice_idx])
+            )  # reverse order of slice_prompt since it's an axial slice from SAR, and add 3d context to put prompt into xyz
 
         coords_RAS = np.array(slice_prompts)
-        prompt_RAS = PromptStep(point_prompts = (coords_RAS, np.ones(len(coords_RAS))))
+        prompt_RAS = PromptStep(point_prompts=(coords_RAS, np.ones(len(coords_RAS))))
         prompt_orig = self.transform_prompt_to_original_coords(prompt_RAS)
         return prompt_orig
 
@@ -188,7 +219,7 @@ class PointPropagationPrompter(Prompter):
         self.n_seed_points_point_propagation = n_seed_points_point_propagation
         self.n_points_propagation = n_points_propagation
 
-        self.promptstep_in_model_coord_system = True # Overwrite default
+        self.promptstep_in_model_coord_system = True  # Overwrite default
 
     def get_prompt(self) -> PromptStep:
         """
@@ -196,7 +227,9 @@ class PointPropagationPrompter(Prompter):
         :return: str (Path to the predicted segmentation)
         """
         seed_points_prompt_RAS = get_seed_point(self.groundtruth_SAR, self.n_seed_points_point_propagation, self.seed)
-        slices_to_infer = np.where(np.any(self.groundtruth_SAR, axis=(1, 2)))[0] # Warning: won't work if set image involves resampling along z dimension
+        slices_to_infer = np.where(np.any(self.groundtruth_SAR, axis=(1, 2)))[
+            0
+        ]  # Warning: won't work if set image involves resampling along z dimension
         seed_points_prompt_orig = self.transform_prompt_to_original_coords(seed_points_prompt_RAS)
 
         all_point_prompts: PromptStep = point_propagation(
@@ -254,7 +287,7 @@ class BoxPropagationPrompter(Prompter):
     def __init__(self, inferer: Inferer, seed: int = 11111):
         super().__init__(inferer, seed)
 
-        self.promptstep_in_model_coord_system = True # Overwrite default
+        self.promptstep_in_model_coord_system = True  # Overwrite default
 
     def get_prompt(self) -> tuple[nib.Nifti1Image, dict[int, np.ndarray]]:
         median_box_seed_prompt_RAS: PromptStep = get_seed_boxes(self.groundtruth_SAR, 1)
@@ -265,11 +298,12 @@ class BoxPropagationPrompter(Prompter):
         return all_boxes_model
 
 
-class NPoints3DVolumePrompter(Prompter):
+class NPoints3DVolumePrompter(Prompter, ABC):
+    n_points: int
 
-    def __init__(self, inferer: Inferer, seed: int = 11111, n_points: int = 5):
+    def __init__(self, inferer: Inferer, seed: int = 11111):
         super().__init__(inferer, seed)
-        self.n_points = n_points
+        self.n_points = self.n_points
 
     def get_prompt(self) -> tuple[nib.Nifti1Image, dict[int, np.ndarray]]:
         prompt_RAS = get_pos_clicks3D(self.groundtruth_SAR, n_clicks=self.n_points, seed=self.seed)
@@ -286,14 +320,23 @@ class Box3DVolumePrompter(Prompter):
 
 
 static_prompt_styles = Literal[
-    "NPointsPer2DSlicePrompter",
+    # ------------------------- 2D Positive Click Prompters ------------------------- #
+    "OneFGPointsPer2DSlicePrompter",
+    "TwoFGPointsPer2DSlicePrompter",
+    "ThreeFGPointsPer2DSlicePrompter",
+    "FiveFGPointsPer2DSlicePrompter",
+    "TenFGPointsPer2DSlicePrompter",
+    "CenterPointPrompter",
+    # -------------------- 2D Point Interpolation and propagation ------------------- #
     "PointInterpolationPrompter",
     "PointPropagationPrompter",
+    # ------------------------------- 2D Box prompters ------------------------------ #
     "BoxPer2DSlicePrompter",
     "BoxPer2dSliceFrom3DBoxPrompter",
+    # --------------------- 2D Box Interpolation and Propagation -------------------- #
     "BoxInterpolationPrompter",
     "BoxPropagationPrompter",
+    # ------------------------- 3D Volume Prompters ------------------------- #
     "NPoints3DVolumePrompter",
     "Box3DVolumePrompter",
-    "CenterPointPrompter"
 ]
