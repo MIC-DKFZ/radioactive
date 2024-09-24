ab827b99d24ef4805b1857ef397b1e812154f1d2
t722s <timothy.tempus@dkfz-heidelberg.de>
2024-09-24 16:36:52 +0200

    added hyperparams for new 3d interactive from 2d algo prompter

--- src/intrab/experiments_runner.py
+++ src/intrab/experiments_runner.py
@@ -49,6 +49,7 @@ if __name__ == "__main__":
         twoD_interactive_n_points_per_slice=1,
         threeD_interactive_n_init_points=1,
         threeD_patch_size=(128, 128, 128),  # This argument is only for SAMMed3D - should be different for segvol
+        threeD_interactive_n_corrective_points = 1
     )
 
     # Potentially move the seeds inside to not create new models each seed, but save that time.
