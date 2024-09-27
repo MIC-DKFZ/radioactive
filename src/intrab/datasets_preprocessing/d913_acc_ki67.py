3c6cc72fefcfe38f12ef672f02ee3e3d641f4d16
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-09-27 11:59:41 +0200

    Add Adrenal kidney preprocessing issues

--- src/intrab/datasets_preprocessing/d913_acc_ki67.py
+++ src/intrab/datasets_preprocessing/d913_acc_ki67.py
@@ -18,8 +18,8 @@ def preprocess(raw_download_dir: Path):
     dicom_dir = raw_download_dir / "adrenal_acc_ki67_seg"
     cases = sorted(os.listdir(str(dicom_dir)))
     matches = {}
-    for case in tqdm(list(cases)):
-        case_path = os.path.join(dicom_dir, case)
+    for case_id in tqdm(list(cases)):
+        case_path = os.path.join(dicom_dir, case_id)
         subfolders = os.listdir(case_path)
         subfolder_with_seg = None
         if len(subfolders) != 1:
@@ -34,10 +34,10 @@ def preprocess(raw_download_dir: Path):
                 elif len(seg_scans) == 0:
                     continue
                 else:
-                    print(f"Case {case} has more than one segmentation!!!")
+                    print(f"Case {case_id} has more than one segmentation!!!")
                     break
                 if folders_with_seg > 1:
-                    print(f"Case {case} has more than one segmentation!!!")
+                    print(f"Case {case_id} has more than one segmentation!!!")
                     break
         if subfolder_with_seg is None:
             subfolder_with_seg = subfolders[0]
@@ -45,7 +45,7 @@ def preprocess(raw_download_dir: Path):
         ct_scans = [s for s in scans if "CT" in s]
         seg_scans = [s for s in scans if "SEG" in s]
         if len(seg_scans) != 1:
-            print(f"Case {case} has more than one segmentation!!!")
+            print(f"Case {case_id} has more than one segmentation!!!")
             continue
 
         # print(f"Case {case} has {len(ct_scans)} CT scans and 1 segmentation")
@@ -63,7 +63,7 @@ def preprocess(raw_download_dir: Path):
             ct = pydicom.dcmread(path)
             # Get Name of scan
             if ct.SeriesInstanceUID == seg_uid:
-                matches[case] = {"ct": path, "seg": seg_path}
+                matches[case_id] = {"ct": path, "seg": seg_path}
                 break
 
     # Go through matches and convert to nifi
@@ -73,15 +73,15 @@ def preprocess(raw_download_dir: Path):
     imagesTr_dir.mkdir(parents=True, exist_ok=True)
     labelsTr_dir.mkdir(parents=True, exist_ok=True)
 
-    for case, paths in matches.items():
-        if (imagesTr_dir / (case + ".nrrd")).exists():
+    for case_id, paths in matches.items():
+        if (imagesTr_dir / (case_id + ".nrrd")).exists():
             continue
 
         ct: tuple[np.ndarray, dict]
         seg: tuple[np.ndarray, dict]
 
         ct_arr, ct_header = dicom_to_nrrd(paths["ct"])
-        nrrd.write(str(imagesTr_dir / f"{case}_0000.nrrd"), ct_arr, ct_header)
+        nrrd.write(str(imagesTr_dir / f"{case_id}_0000.nrrd"), ct_arr, ct_header)
 
         seg_arr, seg_header = dicom_to_nrrd(paths["seg"])
 
@@ -91,6 +91,10 @@ def preprocess(raw_download_dir: Path):
         except Exception as e:
             print(e)
             raise e
+
+        if seg_arr.shape != ct_arr.shape:
+            print(f"Case {case_id} has different shapes for CT and SEG")
+            continue
 
         # ToDo: Add option to remove instance from innrrd through popping bin map values.
         innrrd: InstanceNrrd
@@ -116,4 +120,4 @@ def preprocess(raw_download_dir: Path):
         # Save the new array
         seg_innrrd: InstanceNrrd
         seg_innrrd = InstanceNrrd.from_instance_map(new_array, seg_header, class_name=1)
-        seg_innrrd.to_file(labelsTr_dir / f"{case}.nrrd")
+        seg_innrrd.to_file(labelsTr_dir / f"{case_id}.nrrd")
