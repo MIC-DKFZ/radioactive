84261d3cd27adab6bcb2c31a43a076fe0f5e5fab
t722s <timothy.tempus@dkfz-heidelberg.de>
2024-09-29 19:05:31 +0200

    sam2norm

--- src/intrab/model/model_utils.py
+++ src/intrab/model/model_utils.py
@@ -3,6 +3,7 @@ from typing import Literal, Type, Union
 
 from loguru import logger
 
+from intrab.model.SAM2Norm import SAM2NormInferer
 from intrab.model.SAMnorm import SAMNormInferer
 from intrab.prompts.prompt_hparams import PromptConfig
 from intrab.prompts.prompter import (
@@ -72,7 +73,7 @@ from intrab.model.SAMMed3D import SAMMed3DInferer
 from intrab.model.segvol import SegVolInferer
 from intrab.model.SAM2 import SAM2Inferer
 
-model_registry = Literal["sam", "sam2", "sammed2d", "sammed3d", "sammed3d_turbo", "medsam", "segvol", "samnorm"]
+model_registry = Literal["sam", "sam2", "sammed2d", "sammed3d", "sammed3d_turbo", "medsam", "segvol", "samnorm", "sam2norm"]
 
 inferer_registry: dict[model_registry, Type[Inferer]] = {
     "sam": SAMInferer,
@@ -83,6 +84,7 @@ inferer_registry: dict[model_registry, Type[Inferer]] = {
     "segvol": SegVolInferer,
     "sam2": SAM2Inferer,
     "samnorm": SAMNormInferer,
+    "sam2norm":SAM2NormInferer,
 }
 
 
@@ -95,6 +97,7 @@ checkpoint_registry: dict[model_registry, Path] = {
     "sammed3d_turbo": get_model_path() / "sam_med3d_turbo.pth",
     "sam2": "",
     "samnorm": get_model_path() / "sam_vit_h_4b8939.pth",
+    "sam2norm": ","
 }
 
 
